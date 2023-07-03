05bb07f39f9ff4b812b1bcb1d0187cb32586385c
ilikly <ifinal.io@qq.com>
2023-07-03 16:57:25 +0800

    update spring-boot 3.+

--- final-boot-autoconfigure/src/main/java/org/ifinalframework/boot/autoconfigure/security/FinalSecurityAutoConfiguration.java
+++ final-boot-autoconfigure/src/main/java/org/ifinalframework/boot/autoconfigure/security/FinalSecurityAutoConfiguration.java
@@ -48,9 +48,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
